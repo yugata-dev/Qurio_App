d74034b7772a62ffbc32fb45e866dd16f851a35e
yugata-dev <yugata.dv@gmail.com>
2026-08-22 12:43:16 +0700

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend LIVEN Berjalan!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Backend aktif di http://localhost:${PORT}`);
+});
