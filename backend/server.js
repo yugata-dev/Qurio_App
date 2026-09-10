42a1a370b46fd328e9d8d04abbd37851c321d2c4
Tekno125 <183708672+Tekno125@users.noreply.github.com>
2026-09-10 22:14:20 +0700

    fix: enable cors credentials

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ const corsOptions = {
         } else {
             callback(new Error("Origin tidak diizinkan oleh CORS"))
         }
-    }
+    },
+    credentials: true,
 }
 
 // =====================
