c021447f59d30ae8261471e57dd2c18f677c2291
yugata-dev <yugata.dv@gmail.com>
2026-09-10 21:59:32 +0700

    fix: use database port for postgres

--- backend/src/config/db/connection.js
+++ backend/src/config/db/connection.js
@@ -10,7 +10,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: Number(process.env.PORT) || 5432,
+    port: Number(process.env.DB_PORT) || 5432,
     max: 20, // maksimal 20 koneksi paralel
     idleTimeoutMillis: 30000 // koneksi idle keluar setelah 30 detik
 })
